1c15c9935319ce25d11b40ab4460ef6e4e814925
sbushinskii <sbushinskii@gmail.com>
2016-03-02 00:46:06 +0600

    LOL

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/pro/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/pro/');
+
+// DIR
+define('DIR_APPLICATION', '/Applications/MAMP/htdocs/pro/catalog/');
+define('DIR_SYSTEM', '/Applications/MAMP/htdocs/pro/system/');
+define('DIR_LANGUAGE', '/Applications/MAMP/htdocs/pro/catalog/language/');
+define('DIR_TEMPLATE', '/Applications/MAMP/htdocs/pro/catalog/view/theme/');
+define('DIR_CONFIG', '/Applications/MAMP/htdocs/pro/system/config/');
+define('DIR_IMAGE', '/Applications/MAMP/htdocs/pro/image/');
+define('DIR_CACHE', '/Applications/MAMP/htdocs/pro/system/storage/cache/');
+define('DIR_DOWNLOAD', '/Applications/MAMP/htdocs/pro/system/storage/download/');
+define('DIR_LOGS', '/Applications/MAMP/htdocs/pro/system/storage/logs/');
+define('DIR_MODIFICATION', '/Applications/MAMP/htdocs/pro/system/storage/modification/');
+define('DIR_UPLOAD', '/Applications/MAMP/htdocs/pro/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '123');
+define('DB_DATABASE', 'open_pro');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
